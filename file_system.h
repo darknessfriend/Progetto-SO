944de8469f1c9a6ed88e4254dfa5efc16dff40cc
darknessfriend <leo.rufini.459@gmail.com>
2024-06-01 20:39:18 +0200

    Inizio del progetto. Creazione di un file header in cui definisco le strutture che utilizzerò per gestire le entry in memoria. Aggiungo le librerie necessarie.

---
+++ file_system.h
@@ -0,0 +1,9 @@
+//Importo librerie necessarie ( classiche più quelle utilizzate dal professore )
+#include <unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
