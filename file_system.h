bf26bc16937da683fd4a9c80a402d1b71c0b9d83
darknessfriend <leo.rufini.459@gmail.com>
2024-06-01 20:51:48 +0200

    Aggiungo grandezze fisse del mio filesystem come la dimensione dei file, il numero massimo di blocchi e la dimensione massima del nome. Probabilmente sto trascurando qualche grandezza che definirò in seguito.

--- file_system.h
+++ file_system.h
@@ -1,4 +1,4 @@
-//Importo librerie necessarie ( classiche più quelle utilizzate dal professore )
+// Importo librerie necessarie ( classiche più quelle utilizzate dal professore )
 #include <unistd.h>
 #include <stdio.h>
 #include <assert.h>
@@ -7,3 +7,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+
+
+// Inizio a definire le strutture che saranno utilizzate per la
+// gestione del mio file system.
+
+// Numero massimo di blocchi di memoria che posso utilizzare
+#define MAX_BLOCKS 1024
+// Dimensione di un blocco
+#define BLOCK_SIZE 512
+// Grandezza massima del nome di una directory
+#define NAME_SIZE 32
